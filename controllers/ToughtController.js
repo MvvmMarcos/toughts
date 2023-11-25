633ba4b5719dea461aad4977add55775ed2d755c
Marcos Venancio <mvvmvenancio@gmail.com>
2023-11-25 16:27:07 -0300

    Enviando atualizações

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,18 +1,31 @@
 const Tought = require('../models/Tought');
 const User = require('../models/User');
 
+const {Op} = require('sequelize');//operadores como like
 module.exports = class ToughtController {
 
     static async showToughts(req, res) {
+        let search = '';
+        if(req.query.search){
+            search = req.query.search
+        }
+
         // const toughts = await Tought.findAll({raw:true});
         //ou
-
         const toughtsData = await Tought.findAll({
             include:User,
+            where:{
+                title:{[Op.like]:`%${search}%`}
+            }
         });
         // const toughts = toughtsData.map((result)=> result.dataValues);
         const toughts = toughtsData.map((result)=> result.get({plain:true}));
-        res.render('toughts/home' , {toughts})
+        //contar os pensamentos na busca
+        let toughtsQty = toughts.length;
+        if(toughtsQty === 0){
+            toughtsQty = false;
+        }
+        res.render('toughts/home' , {toughts, search, toughtsQty})
     }
     static createTought(req, res) {
         res.render('toughts/create');
